db471a74ed2ec85c3546c047a56b7cb9e1612d5e
florin_alistar <florin.alistar@yahoo.com>
2024-09-10 16:58:19 +0300

    Dezactivare securitate

--- src/main/java/ro/axon/dot/DotApplication.java
+++ src/main/java/ro/axon/dot/DotApplication.java
@@ -1,9 +1,13 @@
 package ro.axon.dot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 public class DotApplication {
 
 	public static void main(String[] args) {
