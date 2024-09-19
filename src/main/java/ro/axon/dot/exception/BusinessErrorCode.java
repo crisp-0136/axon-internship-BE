12e7a49835921f822bd5f69a27f0ceb7702d81e7
andrei_jeman <jeman.andrei1@gmail.com>
2024-09-19 18:05:13 +0300

    remove INVALID_EMPLOYEE, REQUEST_NOT_FOUND, INVALID_REQUEST from BusinessErrorCode

--- src/main/java/ro/axon/dot/exception/BusinessErrorCode.java
+++ src/main/java/ro/axon/dot/exception/BusinessErrorCode.java
@@ -13,23 +13,8 @@ public enum BusinessErrorCode {
             "Employee was not found",
             HttpStatus.NOT_FOUND),
 
-    INVALID_EMPLOYEE(
-            formatErrorCode("0002", "400"),
-            "Employee data does not correspond to the rules",
-            HttpStatus.BAD_REQUEST),
-
-    REQUEST_NOT_FOUND(
-            formatErrorCode("0003", "404"),
-            "Request was not found",
-            HttpStatus.NOT_FOUND),
-
-    INVALID_REQUEST(
-            formatErrorCode("0004", "400"),
-            "Request data does not correspond to the rules",
-            HttpStatus.BAD_REQUEST),
-
     TEAM_NOT_FOUND(
-            formatErrorCode("0005", "404"),
+            formatErrorCode("0002", "404"),
             "Team was not found",
             HttpStatus.NOT_FOUND);
 
@@ -40,7 +25,7 @@ public enum BusinessErrorCode {
     private static final String ERROR_CODE_PATTERN = "EDOT%s%s";
 
     public static String formatErrorCode(String id, String status) {
-        if(id.length() != 4 && status.length() != 3) {
+        if(id.length() != 4 || status.length() != 3) {
             throw new IllegalArgumentException("Id must be 4 digits and status must be 3 digits.");
         }
         return String.format(ERROR_CODE_PATTERN, id, status);
